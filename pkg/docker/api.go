ef8d9a287a4cd0867d0e5f61eda0b274624d0449
= <herlon214@gmail.com>
2018-05-18 14:05:49 -0400

    Removed types package

--- pkg/docker/api.go
+++ pkg/docker/api.go
@@ -6,8 +6,82 @@ import (
 
 	"github.com/franela/goreq"
 	"github.com/herlon214/gdsc/pkg/logger"
-	"github.com/herlon214/gdsc/pkg/types"
 )
+
+type ModeReplicated struct {
+	Replicas int
+}
+
+type Mode struct {
+	Replicated ModeReplicated
+}
+
+type UpdateConfig struct {
+	Parallelism     int
+	Delay           int
+	FailureAction   string
+	Monitor         int
+	MaxFailureRatio int
+	Order           string
+}
+
+type RollbackConfig struct {
+	Parallelism     int
+	FailureAction   string
+	Monitor         int
+	MaxFailureRatio int
+	Order           string
+}
+
+type Network struct {
+	Target string
+}
+
+type EndpointSpec struct {
+	Mode string
+}
+
+type ContainerSpec struct {
+	Image string
+}
+
+type TaskTemplate struct {
+	ContainerSpec ContainerSpec
+	ForceUpdate   int
+	Runtime       string
+}
+
+// Docker service spec struct
+type Spec struct {
+	Name           string
+	Labels         map[string]string
+	TaskTemplate   TaskTemplate
+	Mode           Mode
+	UpdateConfig   UpdateConfig
+	RollbackConfig RollbackConfig
+	Networks       []Network
+	EndpointSpec   EndpointSpec
+}
+
+type ServiceVersion struct {
+	Index int
+}
+
+// Docker service struct
+type Service struct {
+	Spec    Spec
+	Version ServiceVersion
+}
+
+type ServiceUpdateQueryString struct {
+	version int
+}
+
+// CreateServiceResponse format of docker api response when create a service
+type CreateServiceResponse struct {
+	message string
+	ID      string
+}
 
 type Api struct {
 	ApiUrl string
@@ -42,17 +116,17 @@ func (api *Api) Request(method string, path string, body interface{}) (string, *
 }
 
 // CreateService create a docker service based on the given spec
-func (api *Api) CreateService(spec types.Spec) *types.CreateServiceResponse {
+func (api *Api) CreateService(spec Spec) *CreateServiceResponse {
 	body, _ := api.Request("POST", "/services/create", spec)
 
-	var response types.CreateServiceResponse
+	var response CreateServiceResponse
 	json.Unmarshal([]byte(body), &response)
 
 	return &response
 }
 
 // UpdateService update a docker service based on the given spec
-func (api *Api) UpdateService(service types.Service) bool {
+func (api *Api) UpdateService(service Service) bool {
 	log := logger.DefaultLogger()
 	newVersion := strconv.Itoa(service.Version.Index)
 
@@ -64,10 +138,10 @@ func (api *Api) UpdateService(service types.Service) bool {
 }
 
 // GetService return the service information
-func (api *Api) GetService(nameOrID string) *types.Service {
+func (api *Api) GetService(nameOrID string) *Service {
 	body, _ := api.Request("GET", "/services/"+nameOrID, nil)
 
-	var response types.Service
+	var response Service
 	json.Unmarshal([]byte(body), &response)
 
 	return &response

b4486f8c70238b7058a8da13a7956eb9615195c5
= <herlon214@gmail.com>
2018-05-19 01:08:45 -0400

    Better commands use, thanks to go-arg

--- cmd/gdsc/main.go
+++ cmd/gdsc/main.go
@@ -2,36 +2,35 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"regexp"
-	"strings"
 
+	"github.com/alexflint/go-arg"
 	"github.com/herlon214/gdsc/pkg/docker"
 	"github.com/herlon214/gdsc/pkg/logger"
 )
 
+// Args contain th received args when execute 'gdsc'
 type Args struct {
-	ImageURL        string
-	BranchName      string
-	ServiceCopyName string
-	NewServiceName  string
-	ServiceVersion  int
+	From  string `help:"service that will be cloned if there is no service with the given --name"`
+	Name  string `help:"service name that will be deployed"`
+	Image string `help:"new docker image url"`
 }
 
 // ParseArgs parse the received args
 func ParseArgs() Args {
-	args := os.Args[1:]
+	var args Args
+	arg.MustParse(&args)
 	r := regexp.MustCompile("\\W")
-	var BranchName = r.ReplaceAllString(args[1], "_")
-	var NewServiceName = strings.Replace(args[0], "develop", BranchName, -1)
-	var ImageURL = args[2]
 
-	return Args{
-		ImageURL:        ImageURL,
-		ServiceCopyName: args[0],
-		BranchName:      BranchName,
-		NewServiceName:  NewServiceName,
-	}
+	// Filter the new name to only words
+	args.Name = r.ReplaceAllString(args.Name, "_")
+
+	// Give a new name if not specify one
+	// if args.Name == "" {
+	// 	args.Name = strings.Replace(args.Name, "develop", args.Branch, -1)
+	// }
+
+	return args
 }
 
 func main() {
@@ -44,35 +43,35 @@ func main() {
 	fmt.Printf("%+v\n", args)
 
 	// Try to get a crated service
-	service := api.GetService(args.NewServiceName)
+	service := api.GetService(args.Name)
 
 	// Check if the service is already created
 	if service.Spec.Name == "" {
-		log.Warningf("Service %s not created, creating a new one based on develop...", args.BranchName)
-		service = api.GetService(args.ServiceCopyName)
+		log.Warningf("Service was %s not created yet, creating a new one based on %s...", args.Name, args.From)
+		service = api.GetService(args.From)
 
 		newService := service
 
 		// Change the service informations
-		newService.Spec.TaskTemplate.ContainerSpec.Image = args.ImageURL
-		newService.Spec.Name = args.NewServiceName
-		newService.Spec.Labels["traefik.frontend.rule"] = "Host: " + args.BranchName + ".doare.org"
+		newService.Spec.TaskTemplate.ContainerSpec.Image = args.Image
+		newService.Spec.Name = args.Name
+		newService.Spec.Labels["traefik.frontend.rule"] = "Host: " + args.Name + ".doare.org"
 
 		response := api.CreateService(newService.Spec)
 		log.Debugf("Service created with ID: %s", response.ID)
 	} else { // Update a created service
-		log.Debugf("Updating a existent service with name %s...", args.BranchName)
+		log.Debugf("Updating a existent service with name %s...", args.Name)
 		newService := service
 
 		// Change the service informations
-		newService.Spec.TaskTemplate.ContainerSpec.Image = args.ImageURL
-		newService.Spec.Name = args.NewServiceName
-		newService.Spec.Labels["traefik.frontend.rule"] = "Host " + args.BranchName + ".doare.org"
+		newService.Spec.TaskTemplate.ContainerSpec.Image = args.Image
+		newService.Spec.Name = args.Name
+		newService.Spec.Labels["traefik.frontend.rule"] = "Host " + args.Name + ".doare.org"
 
 		if api.UpdateService(*newService) == true {
-			log.Noticef("Service %s updated successfully!", args.BranchName)
+			log.Noticef("Service %s updated successfully!", args.Name)
 		} else {
-			log.Errorf("Failure when updating service %s!", args.BranchName)
+			log.Errorf("Failure when updating service %s!", args.Name)
 		}
 	}
 
